767f667015e45eae89090d026b783bc21d6d4cba
tamouz <tamouz.hmidan@gmail.com>
2021-09-08 20:54:22 +0300

    removed uneccary service

--- src/app/Guards/token-gaurd.guard.ts
+++ src/app/Guards/token-gaurd.guard.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { CheckIfLogedInService } from '../services/check-if-loged-in.service';
+import { GetTokensService } from '../services/get-tokens.service';
 @Injectable({
   providedIn: 'root'
 })
 export class tokenGaurdGaurd implements CanActivate {
-  constructor(private _router: Router,private token:CheckIfLogedInService) {
+  constructor(private _router: Router,private token:GetTokensService) {
 
   }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot) {
-      if (this.token.isLogged())
+      if (this.token.getClientToken()!="empty")
       { return true; } this._router.navigate(['']); return false; } }
